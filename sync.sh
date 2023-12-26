c1961464d95f764ee50e96f29eaf2ae91a9cc6f7
Gico Carlo Evangelista <evangelistagico@pm.me>
2023-12-26 21:29:13 +0000

    test on pi zero

--- sync.sh
+++ sync.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# sync.sh
+# Uses the "rsync sync" command to sync files in $RSYNC_PATHS
+
 # Internal Field Separator
 IFS=','
 
-# The RSYNC_PATHS env variable should be a comma separated string
-read -ra array <<< "$RSYNC_PATHS"
+# The RSYNC_SOURCE_PATHS env variable should be a comma separated string
+read -ra array <<< "$RSYNC_SOURCE_PATHS"
 
+# TODO: set up "rsync sync"
 for element in "${array[@]}"; do
     echo "$element"
 done
